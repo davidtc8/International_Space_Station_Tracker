2cce2ccb5dee5996fa265eb06b0ac9f24a17ff03
David Torres <davidtorresc8@gmail.com>
2021-02-20 11:13:11 -0600

    ISS Code!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from datetime import datetime
 import random
 import smtplib
+import time
 
 # TODO: Remember that only you can see your password
 ###### Constants ########
@@ -83,16 +84,24 @@ iss_close = iss_is_close_to_me()
 is_night = is_night_time()
 
 ###### Sending me an email if the previous functions are True ########
-#If the ISS is close to my current position
-if iss_close is True and is_night is True:
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        # tls stands for transport layer security and is a way of securing our connection to our email server
-        connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(from_addr=my_email,
-                            to_addrs= recipient,
-                            msg="Subject: The International Space Station is near you!\n\nTake a look at the sky, you will have the chance to take a look at the space station\n\n Keep dreaming (:")
-    #confirmation email
-    print(f"I have sent you an email to {recipient}, go and take a look at it!")
-else:
-    print("The space station is nowhere near you")
+is_iss_there = True
+while is_iss_there:
+    time.sleep(180)
+    minute = 0
+    iteration = 0
+    #If the ISS is close to my current position
+    if iss_close is True and is_night is True:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            # tls stands for transport layer security and is a way of securing our connection to our email server
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs= recipient,
+                                msg="Subject: The International Space Station is near you!\n\nTake a look at the sky, you will have the chance to take a look at the space station\n\n Keep dreaming (:")
+        #confirmation email
+        print(f"I have sent you an email to {recipient}, go and take a look at it!")
+        print(f"I have waited {minute / 60} hours for this to complete, so you better enjoy it")
+    else:
+        iteration += 1
+        minute += 5
+        print(f"Iteration number {iteration}... The space station is nowhere near you, {minute} minutes have passed")
